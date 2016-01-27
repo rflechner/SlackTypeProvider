e09b98d24fa0893d0be7e142d8437f9dc713d8c4
rflechner <rflechner@romcyber.com>
2016-01-28 00:11:56 +0100

    start doc

--- src/SlackTypeProvider/Script.fsx
+++ src/SlackTypeProvider/Script.fsx
@@ -7,11 +7,9 @@ open SlackProvider
 
 type TSlack = SlackTypeProvider<token="C:/keys/slack_token.txt">
 let slack = TSlack()
-let channels = slack.Channels
-let all = channels.GetAll()
 
-slack.Users.romain_flechner.Send("Hi")
-slack.Users.romain_flechner.Send("Hi 2",botname="robot 4", iconUrl="http://statics.romcyber.com/icones/robot1_48x48.jpg")
+slack.Users.romain_flechner.Send("Hi !")
+slack.Users.romain_flechner.Send("I am a bot",botname="robot 4", iconUrl="http://statics.romcyber.com/icones/robot1_48x48.jpg")
 
-
-
+//let channels = slack.Channels
+//let all = channels.GetAll()
