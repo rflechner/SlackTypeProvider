f5f82057ccb5cc59ca92f5a15abdecd54caafff7
rflechner <rflechner@romcyber.com>
2016-01-27 23:35:04 +0100

    scaffold

---
+++ src/SlackTypeProvider/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("SlackTypeProvider")>]
+[<assembly: AssemblyProductAttribute("SlackTypeProvider")>]
+[<assembly: AssemblyDescriptionAttribute("A tiny type provider to use Slack API")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
