251a5276d0e81d1fc4a002c4905ce57fe8998125
rflechner <rflechner@romcyber.com>
2017-07-13 16:38:52 +0200

    TASK: migrate projects to 4.5.2

--- src/SlackTypeProvider/AssemblyInfo.fs
+++ src/SlackTypeProvider/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("SlackTypeProvider")>]
 [<assembly: AssemblyProductAttribute("SlackTypeProvider")>]
 [<assembly: AssemblyDescriptionAttribute("A tiny type provider to use Slack API")>]
-[<assembly: AssemblyVersionAttribute("0.0.6")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.6")>]
+[<assembly: AssemblyVersionAttribute("0.0.7")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.6"
+    let [<Literal>] Version = "0.0.7"
